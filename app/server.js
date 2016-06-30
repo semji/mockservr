c9d91888a2f9b3db0a7241da58c7525d3acdb777
cyril <perraud.cyril@gmail.com>
2016-06-30 23:21:35 +0200

    Add mocks parser

--- app/server.js
+++ app/server.js
@@ -1,6 +1,11 @@
+var Parser = require('./modules/parser');
 var http = require('http');
 
+var apiParser = new Parser('./mocks/');
+apiParser.parse();
+
 var server = http.createServer(function(req, res) {
+
     res.writeHead(200, {"Content-Type": "text/html"});
     res.end('<p>Voici un paragraphe <strong>HTML</strong> !</p>');
 });
