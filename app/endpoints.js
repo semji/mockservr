eacb6a0426f172025bf9dc21f7ae1a04e6bf09b8
nicolas.couet <nicolas.couet@reputationvip.com>
2017-05-05 14:59:34 +0200

    update examples

--- app/endpoints.js
+++ app/endpoints.js
@@ -1,31 +1,38 @@
 module.exports = [
     {
-        uri: '/internetbs/domain/Check',
+        uri: '/hello-world.html',
         status: 200,
         time: 0,
-        body: `
-#set($random=$math.random())
-#set($random=$random * 10)
-#set($random=$math.round($random))
-$req.query.Domain
-transactid=b827569b003d1b3b6b4f373074f480d0
-status=AVAILABLE
-domain=tytytopol.com
-minregperiod=1Y
-maxregperiod=10Y
-registrarlockallowed=YES
-privatewhoisallowed=YES
-realtimeregistration=YES
-price_ispremium=NO
-            
-transactid=16fc8c8c7eda0d2fe383d1ed38906604
-status=UNAVAILABLE
-domain=google.fr`,
+        body: '<h1>Hello World!</h1>',
         headers: {
-            'Content-Type': 'text/plain; charset=utf-8'
+            'Content-Type': 'text/html; charset=UTF-8',
         },
         velocity: {
-            enabled: true
+            enabled: true,
+        }
+    },
+    {
+        uri: '/hello-world.json',
+        status: 200,
+        time: 0,
+        body: '{"hello": "world !"}',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        velocity: {
+            enabled: true,
+        }
+    },
+    {
+        uri: '/hello/:world',
+        status: 200,
+        time: 0,
+        body: '<h1>Hello $params.world!</h1>',
+        headers: {
+            'Content-Type': 'text/html; charset=UTF-8',
+        },
+        velocity: {
+            enabled: true,
         }
     }
 ];
