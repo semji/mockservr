e197d742a373b2fef1ed304cf0d9dd00eea8c6bc
Alban Pommeret <ap@reputationvip.com>
2018-08-20 16:26:39 +0200

    Configure GUI theme

---
+++ app/gui/src/theme/index.js
@@ -0,0 +1,22 @@
+const theme = {
+  colors: {
+    green: '#8EC5AB',
+    yellow: '#C0C58E',
+    blue: '#92C3E0',
+    blueGrey: '#4A585B',
+    grey: '#3A3C41',
+    darkBlueGrey: '#293031',
+    darkGrey: '#2C2E31',
+    transparentWhite: 'rgba(0, 0, 0, 0.2)',
+    transparentDark: 'rgba(255, 255, 255, 0.1)',
+    white: 'white',
+  },
+  text: {
+    font: 'Oswald, sans-serif',
+  },
+};
+
+theme.colors.primary = theme.colors.blue;
+theme.colors.background = theme.colors.grey;
+
+export default theme;
